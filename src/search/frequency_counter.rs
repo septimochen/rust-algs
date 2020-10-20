85bee15a3ec6e962fe2f2700edeca98af3eb1414
septimochen <qizu.chen@hotmail.com>
2020-10-20 11:02:38 +0000

    freq counter

--- src/search/frequency_counter.rs
+++ src/search/frequency_counter.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
-// use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
-// use std::{cell::RefCell};
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -23,18 +21,13 @@ where
     let mut word_str = String::new();
     if let Ok(lines) = read_lines(filepath) {
         for line in lines {
-            if let Ok(l) = line {
-                // println!("{}", l);
-                let line_words = l.split_whitespace();
-                // println!("{:?}", line_words);
+            if let Ok(line) = line {
+                let line_words = line.split_whitespace();
                 for word in line_words {
-                    // println!("{}", word);
                     word_str = word.to_string();
-                    // println!("{:?}", word_str);
                     if word_str.len() < minlen as usize {
                         continue;
                     } else if st.get(&word_str) == None {
-                        // println!("word {}", word);
                         st.insert(word_str, 1);
                     } else {
                         *st.get_mut(&word_str).unwrap() += 1;
@@ -45,7 +38,6 @@ where
     }
     let mut max_str = String::from("");
     st.insert(max_str.clone(), 0);
-    // println!("{:?}", st);
     for key in st.keys() {
         if st.get(key) > st.get(&max_str) {
             max_str = key.clone();

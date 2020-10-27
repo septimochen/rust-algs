92514efc8ead551b26fdf622de66054da91827d8
septimochen <qizu.chen@hotmail.com>
2020-10-27 05:36:11 +0000

    st trait

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -5,3 +5,21 @@ pub mod frequency_counter;
 pub mod node;
 pub mod binary_search_st;
 pub mod bst;
+
+pub trait ST<K, V> {
+    fn new() -> Self;
+
+    fn put(&mut self, key: K, val: V);
+
+    fn get(&self, key: &K) -> Option<V>;
+
+    fn delete(&mut self, key: &K);
+
+    fn contains(&self, key: &K) -> bool {
+        self.get(key).is_some()
+    }
+
+    fn is_empty(&self) -> bool;
+
+    fn size(&self) -> usize;
+}

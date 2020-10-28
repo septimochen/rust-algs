641a605ce3b1412e31a1f7b1d169c9466e9ba24f
septimochen <qizu.chen@hotmail.com>
2020-10-28 15:29:28 +0800

    format

--- src/search/frequency_counter.rs
+++ src/search/frequency_counter.rs
@@ -124,11 +124,6 @@ where
                     word_str = word.to_string();
                     if word_str.len() < minlen as usize {
                         continue;
-                    // } else if st.get(&word_str) == None {
-                    //     st.insert(word_str, 1);
-                    // } else {
-                    //     *st.get_mut(&word_str).unwrap() += 1;
-                    // }
                     } else {
                         // println!("{}", word);
                         match st.get(word.to_string()) {
@@ -139,7 +134,6 @@ where
                                 st.put(word_str.to_string(), x + 1);
                             }
                         }
-                        // println!("{:?}", st);
                     }
                 }
             }

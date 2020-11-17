bc2d16623418966b5861b766462d71711cd2c9ce
septimochen <qizu.chen@hotmail.com>
2020-11-17 08:32:09 +0000

    update sort

--- src/sorts/mod.rs
+++ src/sorts/mod.rs
@@ -6,3 +6,26 @@ pub trait Sorter<T> {
     fn sort(&self, slice: &mut [T])
     where T: Ord;
 }
+
+pub struct StdSorter;
+impl<T> Sorter<T> for StdSorter {
+    fn sort(&self, slice: &mut [T]) 
+    where 
+    T: Ord,
+    {
+        slice.sort();
+    }
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn std_works() {
+        let mut things = vec![4, 2, 3, 1];
+        StdSorter.sort(&mut things);
+        assert_eq!(things, &[1, 2, 3, 4]);
+    }
+}

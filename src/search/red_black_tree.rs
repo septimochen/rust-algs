c3bcb728fc34a39fda30693da583dad558bebdba
septimochen <qizu.chen@hotmail.com>
2020-11-24 21:59:12 +0800

    update

--- src/search/red_black_tree.rs
+++ src/search/red_black_tree.rs
@@ -11,3 +11,12 @@ pub enum Color {
     Red,
     Black
 }
+
+pub struct Node<K, V> {
+    pub key: K,
+    pub val: V,
+    pub left: Option<Box<Node<K, V>>>,
+    pub right: Option<Box<Node<K, V>>>,
+    pub color: Color,
+}
+

5f7dc07f593cbc04f2313ba16c2674eb8004aa88
septimochen <qizu.chen@hotmail.com>
2020-11-29 21:20:07 +0800

    update insertion sort

--- src/sorts/insertion_sort.rs
+++ src/sorts/insertion_sort.rs
@@ -18,15 +18,24 @@ pub fn insertion_sort<T: Ord>(arr: &mut [T]) -> &[T] {
 pub struct InsertionSort;
 
 impl<T> Sorter<T> for InsertionSort {
-    fn sort<T>(&self, slice: &mut [T])
+    fn sort(&self, slice: &mut [T])
     where
         T: Ord,
     {
+        for unsorted in 1..slice.len() {
+            let mut i = unsorted;
+            while i > 0 && slice[i - 1] > slice[i] {
+                slice.swap(i - 1, i);
+                i -= 1;
+            }
+        }
     }
 }
 #[test]
 pub fn insertion_run() {
     let mut a = [1, 2, 3, 4, -1, 3, 2, 1];
     let b = insertion_sort(&mut a);
-    println!("{:?}", b)
+    println!("{:?}", b);
+    InsertionSort.sort(&mut a);
+    assert_eq!(a, [-1, 1, 1, 2, 2, 3, 3, 4]);
 }

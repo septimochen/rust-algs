da99c653a993704d1a81fa0709de38094cea2b06
septimochen <qizu.chen@hotmail.com>
2020-11-29 20:37:23 +0800

    update insertion sort

--- src/sorts/insertion_sort.rs
+++ src/sorts/insertion_sort.rs
@@ -1,3 +1,5 @@
+use super::Sorter;
+
 #[allow(dead_code)]
 pub fn insertion_sort<T: Ord>(arr: &mut [T]) -> &[T] {
     let n = arr.len();
@@ -13,6 +15,15 @@ pub fn insertion_sort<T: Ord>(arr: &mut [T]) -> &[T] {
     arr
 }
 
+pub struct InsertionSort;
+
+impl<T> Sorter<T> for InsertionSort {
+    fn sort<T>(&self, slice: &mut [T])
+    where
+        T: Ord,
+    {
+    }
+}
 #[test]
 pub fn insertion_run() {
     let mut a = [1, 2, 3, 4, -1, 3, 2, 1];

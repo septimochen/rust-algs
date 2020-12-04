ea87831eb9b4a7ba6319b65206da9df5b6c4ee83
septimochen <qizu.chen@hotmail.com>
2020-12-04 23:07:00 +0800

    update for leetcode

--- src/dynamic_programming/fibonacci.rs
+++ src/dynamic_programming/fibonacci.rs
@@ -3,20 +3,25 @@ pub struct Fibonacci;
 
 #[allow(dead_code)]
 impl Fibonacci {
-    pub fn fib(&self, n: usize) -> usize {
-        if n <= 2 {
+    pub fn fib(&self, n: i32) -> i32 {
+        if n <= 0 {
+            return 0;
+        } else if n <= 2 {
             return 1;
         } else {
             self.fib(n - 1) + self.fib(n - 2)
         }
     }
 
-    pub fn fib_with_helper(&self, n: usize) -> usize {
-        let mut memo = vec![0; n + 1];
-        return self.helper(&mut memo, n);
+    pub fn fib_with_helper(&self, n: i32) -> i32 {
+        if n <= 0 {
+            return 0;
+        }
+        let mut memo = vec![0; (n + 1) as usize];
+        return self.helper(&mut memo, n as usize);
     }
 
-    fn helper(&self, memo: &mut Vec<usize>, n: usize) -> usize {
+    fn helper(&self, memo: &mut Vec<i32>, n: usize) -> i32 {
         //base case
         if n <= 2 {
             return 1;
@@ -36,7 +41,7 @@ fn fib_test() {
     let x = Fibonacci.fib_with_helper(6);
     let y = Fibonacci.fib(6);
     assert_eq!(x, 8);
-    assert_eq!(y, x);
+    assert_eq!(y, x as i32);
     let z = Fibonacci.fib(7);
     assert_eq!(z, 13);
 }

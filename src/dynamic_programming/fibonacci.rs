6c108df338476a000a75a75dbe64e1bc9114e100
septimochen <qizu.chen@hotmail.com>
2020-12-04 07:22:23 +0000

    fib 2

--- src/dynamic_programming/fibonacci.rs
+++ src/dynamic_programming/fibonacci.rs
@@ -7,8 +7,29 @@ pub fn fib(n: usize) -> usize {
     }
 }
 
+#[allow(dead_code)]
+pub fn fib_with_helper(n: usize) -> usize {
+    let mut memo = vec![0; n + 1];
+    return helper(&mut memo, n);
+}
+
+#[allow(dead_code)]
+fn helper(memo: &mut Vec<usize>, n: usize) -> usize {
+    //base case
+    if n <= 2 {
+        return 1;
+    } else {
+        if memo[n] != 0 {
+            return memo[n];
+        } else {
+            memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+            return memo[n];
+        }
+    }
+}
+
 #[test]
 fn fib_test() {
-    let x = fib(4);
+    let x = fib_with_helper(4);
     assert_eq!(x, 3);
 }

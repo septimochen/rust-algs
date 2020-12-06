a26dea4f72d106a0ea98375ad768605cc395b8cc
septimochen <qizu.chen@hotmail.com>
2020-12-06 13:10:10 +0000

    quick

--- src/sorts/quick_sort.rs
+++ src/sorts/quick_sort.rs
@@ -14,7 +14,37 @@ fn quick_sort<T: Ord>(slice: &mut [T]) {
         _ => {}
     }
 
+    let (pivot, rest) = slice.split_first_mut().expect("slice is non-empty");
+    let mut left = 0;
+    let mut right = rest.len() -1 ;
+    while left <= right {
+        if &rest[left] <= pivot {
+            left += 1;
+        } else if &rest[right] > pivot {
+            if right == 0 {
+                break;
+            }
+            right -= 1;
+        } else {
+            rest.swap(left, right);
+            left += 1;
+            if right == 0 {
+                break;
+            }
+            right -= 1;
+        }
+    }
+        // re-align left to account for the pivot at 0
+        let left = left + 1;
+
+        // place the pivot at its final location
+        slice.swap(0, left - 1);
     
+        // split_at_mut(mid: usize) -> (&mut [..mid), &mut [mid..])
+        let (left, right) = slice.split_at_mut(left - 1);
+        assert!(left.last() <= right.first());
+        quick_sort(left);
+        quick_sort(&mut right[1..]);
 }
 
 impl<T> Sorter<T> for QuickSort {

4d7ac196f6623ef4e2d1c49fe8eae1653fa0a280
septimochen <qizu.chen@hotmail.com>
2020-12-06 13:09:50 +0000

    quick

--- src/sorts/quick_sort.rs
+++ src/sorts/quick_sort.rs
@@ -2,7 +2,20 @@ use super::Sorter;
 
 pub struct QuickSort;
 
-fn quick_sort<T: Ord>(slice: &mut [T]) {}
+fn quick_sort<T: Ord>(slice: &mut [T]) {
+    match slice.len() {
+        0 | 1 => return,
+        2 => {
+            if slice[0] > slice[1] {
+                slice.swap(0, 1);
+            }
+            return
+        },
+        _ => {}
+    }
+
+    
+}
 
 impl<T> Sorter<T> for QuickSort {
     fn sort(&self, slice: &mut [T])

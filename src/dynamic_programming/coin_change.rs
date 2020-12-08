8f6cc7d15d4b132aba9c72ac2b9ccf0d41158248
septimochen <qizu.chen@hotmail.com>
2020-12-08 07:24:30 +0000

    coin change fix

--- src/dynamic_programming/coin_change.rs
+++ src/dynamic_programming/coin_change.rs
@@ -2,11 +2,11 @@ use std::cmp::min;
 use std::cmp::Ordering;
 
 #[allow(dead_code)]
-pub struct CoinChange {}
+pub struct CoinChange;
 
 #[allow(dead_code)]
 impl CoinChange {
-    fn coin_change(&self, coins: Vec<i32>, amount: i32) -> i32 {
+    pub fn coin_change(&self, coins: Vec<i32>, amount: i32) -> i32 {
         fn dp(n: i32, coins: &Vec<i32>, amount: &i32) -> i32 {
             let cmp = n.partial_cmp(&0).unwrap();
             let mut res = amount + 1;
@@ -19,12 +19,22 @@ impl CoinChange {
                         if sub_problem == -1 {
                             continue;
                         }
-                        res = min(res, sub_problem);
+                        res = min(res, 1 + sub_problem);
                     }
-                    return res;
+                    res
                 }
             }
         }
         dp(amount, &coins, &amount)
+    }
+}
+
+#[cfg(test)]
+mod coin_tests {
+    use super::*;
+    #[test]
+    fn coin_works_1() {
+        let x = CoinChange.coin_change(vec![1, 2, 5], 11);
+        assert_eq!(x, 3);
     }
 }

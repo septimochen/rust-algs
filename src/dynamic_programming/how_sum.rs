94bf2f50488f5a143df95c3a90640a63eb4bdde7
septimochen <qizu.chen@hotmail.com>
2020-12-29 08:24:28 +0000

    how sum

--- src/dynamic_programming/how_sum.rs
+++ src/dynamic_programming/how_sum.rs
@@ -1,1 +1,50 @@
+use std::collections::HashMap;
+#[allow(dead_code)]
+pub struct HowSum;
 
+#[allow(dead_code)]
+impl HowSum {
+    pub fn how_sum(target_sum: i32, numbers: &Vec<i32>) -> Option<Vec<i32>> {
+        let mut memo: HashMap<i32, Option<Vec<i32>>> = HashMap::new();
+        HowSum::helper(target_sum, numbers, &mut memo)
+    }
+
+    pub fn helper(
+        target_sum: i32,
+        numbers: &Vec<i32>,
+        memo: &mut HashMap<i32, Option<Vec<i32>>>,
+    ) -> Option<Vec<i32>> {
+        if memo.contains_key(&target_sum) {
+            return memo[&target_sum].clone();
+        }
+        if target_sum == 0 {
+            return Some(vec![]);
+        }
+        if target_sum < 0 {
+            return None;
+        }
+        for &num in numbers {
+            let remainder = target_sum - num;
+            let remainder_result = HowSum::helper(remainder, numbers, memo);
+            if remainder_result.is_some() {
+                let mut result = vec![num];
+                result.append(&mut remainder_result.unwrap());
+                memo.insert(target_sum, Some(result.clone()));
+                return Some(result.clone());
+            }
+        }
+        memo.insert(target_sum, None);
+        return None;
+    }
+}
+
+#[cfg(test)]
+mod how_sum_test {
+    use super::*;
+
+    #[test]
+    fn how_sum_test_1() {
+        let a = HowSum::how_sum(7, &vec![5, 3, 4]);
+        println!("{:?}", a);
+    }
+}

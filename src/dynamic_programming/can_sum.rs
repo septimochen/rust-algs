39a2c45ff7c0f0c0dcf0e5ad2e0f0d310f8a0682
septimochen <qizu.chen@hotmail.com>
2020-12-29 02:19:22 +0000

    can sum

--- src/dynamic_programming/can_sum.rs
+++ src/dynamic_programming/can_sum.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
 #[allow(dead_code)]
 pub struct CanSum;
 
 #[allow(dead_code)]
 impl CanSum {
     pub fn can_sum(target_sum: i32, numbers: &Vec<i32>) -> bool {
+        let mut memo: HashMap<i32, bool> = HashMap::new();
+        CanSum::helper(target_sum, numbers, &mut memo)
+    }
+
+    pub fn helper(target_sum: i32, numbers: &Vec<i32>, memo: &mut HashMap<i32, bool>) -> bool {
         if target_sum == 0 {
             return true;
         }
@@ -11,13 +17,18 @@ impl CanSum {
             return false;
         }
         for &num in numbers {
+            if memo.contains_key(&target_sum) {
+                return memo[&target_sum];
+            }
             let remainder = target_sum - num;
-            if CanSum::can_sum(remainder, numbers) == true {
+            if CanSum::helper(remainder, numbers, memo) == true {
+                memo.insert(target_sum, true);
                 return true;
             } else {
                 continue;
             }
         }
+        memo.insert(target_sum, false);
         return false;
     }
 }
@@ -36,7 +47,7 @@ mod can_sum_test {
         assert_eq!(c, false);
         let d = CanSum::can_sum(8, &vec![2, 3, 5]);
         assert_eq!(d, true);
-        // let e = CanSum::can_sum(300, &vec![7, 14]);
-        // assert_eq!(e, false);
+        let e = CanSum::can_sum(300, &vec![7, 14]);
+        assert_eq!(e, false);
     }
 }

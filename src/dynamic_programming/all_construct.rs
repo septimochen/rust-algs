ad79760b305b3e9ff958dca79e4ba085b374d1b1
septimochen <qizu.chen@hotmail.com>
2020-12-30 09:48:47 +0000

    all construct

--- src/dynamic_programming/all_construct.rs
+++ src/dynamic_programming/all_construct.rs
@@ -1,5 +1,21 @@
+use std::collections::HashMap;
+
 #[allow(dead_code)]
-pub fn helper<'a>(target: &str, word_bank: &Vec<&'a str>) -> Vec<Vec<&'a str>> {
+pub fn all_construct<'a>(target: &'a str, word_bank: &Vec<&'a str>) -> Vec<Vec<&'a str>> {
+    let mut memo: HashMap<&str, Vec<Vec<&str>>> = HashMap::new();
+    return helper(target, word_bank, &mut memo);
+}
+
+#[allow(dead_code)]
+pub fn helper<'a>(
+    target: &'a str,
+    word_bank: &Vec<&'a str>,
+    memo: &mut HashMap<&'a str, Vec<Vec<&'a str>>>,
+) -> Vec<Vec<&'a str>> {
+    if memo.contains_key(target) {
+        return memo.get(target).unwrap().to_vec();
+    }
+
     if target.is_empty() {
         return vec![vec![]];
     }
@@ -10,15 +26,16 @@ pub fn helper<'a>(target: &str, word_bank: &Vec<&'a str>) -> Vec<Vec<&'a str>> {
     for &word in word_bank {
         if target.starts_with(word) {
             let suffix = target.strip_prefix(word).unwrap_or("");
-            let suffix_ways = helper(suffix, word_bank);
+            let suffix_ways = helper(suffix, word_bank, memo);
             let mut target_ways = vec![];
             for mut x in suffix_ways {
-                x.push(word);
+                x.insert(0, word);
                 target_ways.push(x);
             }
             result.push(target_ways.into_iter().flatten().collect());
         }
     }
+    memo.insert(target, result.clone());
     return result;
 }
 
@@ -28,13 +45,13 @@ mod best_sum_test {
 
     #[test]
     fn acllstruct_test() {
-        let a = helper("abcdef", &vec!["ab", "abc", "cd", "def", "abcd", "ef"]);
+        let a = all_construct("abcdef", &vec!["ab", "abc", "cd", "def", "abcd", "ef"]);
         assert_eq!(
             a,
             vec![
-                vec!["ef", "cd", "ab"],
-                vec!["def", "abc"],
-                vec!["ef", "abcd"]
+                vec!["ab", "cd", "ef"],
+                vec!["abc", "def"],
+                vec!["abcd", "ef"]
             ]
         );
     }

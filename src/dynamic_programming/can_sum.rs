6975825dc27e826047c4ba1147ecb8fce96b37dd
septimochen <qizu.chen@hotmail.com>
2021-01-02 15:23:04 +0000

    can sum tabulation

--- src/dynamic_programming/can_sum.rs
+++ src/dynamic_programming/can_sum.rs
@@ -31,6 +31,23 @@ impl CanSum {
         memo.insert(target_sum, false);
         return false;
     }
+
+    pub fn can_sum_2(target_sum: i32, numbers: &Vec<i32>) -> bool {
+        let key = target_sum as usize;
+        let mut table = vec![false; (target_sum + 1) as usize];
+        table[0] = true;
+        for i in 0..=key {
+            if table[i] == true {
+                for num in numbers {
+                    let next = (i as i32 + num) as usize;
+                    if next <= key {
+                        table[next] = true;
+                    }
+                }
+            }
+        }
+        table[key]
+    }
 }
 
 #[cfg(test)]
@@ -48,6 +65,20 @@ mod can_sum_test {
         let d = CanSum::can_sum(8, &vec![2, 3, 5]);
         assert_eq!(d, true);
         let e = CanSum::can_sum(14000, &vec![7, 14]);
+        assert_eq!(e, true);
+    }
+
+    #[test]
+    fn can_sum_test_2() {
+        let a = CanSum::can_sum_2(7, &vec![2, 3]);
+        assert_eq!(a, true);
+        let b = CanSum::can_sum_2(7, &vec![5, 3, 4, 7]);
+        assert_eq!(b, true);
+        let c = CanSum::can_sum_2(7, &vec![2, 4]);
+        assert_eq!(c, false);
+        let d = CanSum::can_sum_2(8, &vec![2, 3, 5]);
+        assert_eq!(d, true);
+        let e = CanSum::can_sum_2(14000, &vec![7, 14]);
         assert_eq!(e, true);
     }
 }

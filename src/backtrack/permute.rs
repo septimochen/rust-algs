84aac3e52952e8caffbcf37ddb7d825fb100ef17
septimochen <qizu.chen@hotmail.com>
2021-01-12 14:12:25 +0000

    permute

--- src/backtrack/permute.rs
+++ src/backtrack/permute.rs
@@ -23,3 +23,9 @@ fn backtrack(nums: &Vec<i32>, track: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
         track.pop();
     }
 }
+
+#[test]
+fn permute_test() {
+    let a = permute(vec![1, 2, 3]);
+    println!("{:?}", a);
+}

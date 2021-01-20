5d2646c853047bfb98d2e1d54468e96dff57a810
septimochen <qizu.chen@hotmail.com>
2021-01-20 13:52:38 +0000

    fund bag add

--- src/fundamentals/bag.rs
+++ src/fundamentals/bag.rs
@@ -1,20 +1,26 @@
 use std::fmt;
 
+#[allow(dead_code)]
 pub struct Node<T> {
     val: T,
     next: Option<Box<Node<T>>>,
 }
 
+#[allow(dead_code)]
 pub struct Bag<T> {
     s: Option<Box<Node<T>>>,
     n: usize,
 }
 
+#[allow(dead_code)]
 impl<T> Bag<T> {
     pub fn new() -> Bag<T> {
-        Bag {
-            s: None,
-            n: 0
-        }
+        Bag { s: None, n: 0 }
+    }
+
+    pub fn add(&mut self, val: T) {
+        let next = self.s.take();
+        self.s = Some(Box::new(Node { val, next }));
+        self.n += 1;
     }
 }

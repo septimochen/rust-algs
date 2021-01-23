ea04dd9b9af398ada5d775f4eee6f99e37914d8d
septimochen <qizu.chen@hotmail.com>
2021-01-23 13:46:07 +0000

    fmt

--- src/fundamentals/bag.rs
+++ src/fundamentals/bag.rs
@@ -16,6 +16,19 @@ impl<T: Clone> Clone for Node<T> {
 }
 
 #[allow(dead_code)]
+fn write_node_to_formatter<T: fmt::Debug>(
+    f: &mut fmt::Formatter,
+    x: Option<&Box<Node<T>>>,
+) -> fmt::Result {
+    if let Some(node) = x {
+        write!(f, "{:?}", node.val)?;
+        write_node_to_formatter(f, node.next.as_ref())
+    } else {
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
 pub struct Bag<T> {
     s: Option<Box<Node<T>>>,
     n: usize,

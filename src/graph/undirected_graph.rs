1239dae4f9489d6817b7fca01c3e85aafef0ac33
septimochen <qizu.chen@hotmail.com>
2021-01-26 05:26:05 +0000

    graph

--- src/graph/undirected_graph.rs
+++ src/graph/undirected_graph.rs
@@ -8,10 +8,11 @@ pub struct Graph {
     adj: Vec<Bag<usize>>,
 }
 
+#[allow(dead_code)]
 impl Graph {
     pub fn new(v: usize) -> Graph {
         Graph {
-            v: v,
+            v,
             e: 0,
             adj: iter::repeat(Bag::<usize>::new()).take(v).collect(),
         }

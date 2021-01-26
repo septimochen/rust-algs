4bdbf0c8a43bcbb53d932a105002df7c86eba03d
septimochen <qizu.chen@hotmail.com>
2021-01-26 05:24:08 +0000

    graph

--- src/graph/undirected_graph.rs
+++ src/graph/undirected_graph.rs
@@ -1,8 +1,19 @@
 use crate::fundamentals::bag::Bag;
+use std::iter;
 
 #[derive(Clone, Debug)]
 pub struct Graph {
     v: usize,
     e: usize,
     adj: Vec<Bag<usize>>,
+}
+
+impl Graph {
+    pub fn new(v: usize) -> Graph {
+        Graph {
+            v: v,
+            e: 0,
+            adj: iter::repeat(Bag::<usize>::new()).take(v).collect(),
+        }
+    }
 }

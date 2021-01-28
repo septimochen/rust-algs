9556bc6d741811ed099186373c1ab83564d6bf28
septimochen <qizu.chen@hotmail.com>
2021-01-28 02:48:12 +0000

    graph bfs, open lock

--- src/graph/open_lock.rs
+++ src/graph/open_lock.rs
@@ -1,7 +1,16 @@
+use std::collections::HashSet;
+
 pub struct Solution;
 
 impl Solution {
     pub fn open_lock(deadends: Vec<String>, target: String) -> i32 {
-        0
+        let mut deads: HashSet<String> = deadends.into_iter().collect();
+        let mut visited: HashSet<String> = HashSet::new();
+        let mut q:Vec<String> = Vec::new();
+        let mut step = 0;
+        q.push("0000".into());
+        visited.insert("0000".into());
+        
+        return step
     }
 }

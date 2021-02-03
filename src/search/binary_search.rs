b224dc83bc1f4bea712cae37311514c7b4ea205e
septimochen <qizu.chen@hotmail.com>
2021-02-03 03:37:02 +0000

    left

--- src/search/binary_search.rs
+++ src/search/binary_search.rs
@@ -22,38 +22,39 @@ pub fn left_bound<T: PartialOrd + PartialEq>(item: &T, arr: &[T]) -> i32 {
         return -1;
     }
     let mut left = 0;
-    let mut right = arr.len();
-    while left < right {
+    let mut right = arr.len() - 1;
+    while left <= right {
         let mid = (left + right) / 2;
         if &arr[mid] == item {
-            right = mid;
+            right = mid - 1;
         } else if &arr[mid] > item {
-            right = mid;
+            right = mid - 1;
         } else {
             left = mid + 1;
         }
     }
-    if left < arr.len() {
-        left as i32
-    } else {
-        -1
+    if left >= arr.len() || &arr[left] != item {
+        return -1;
     }
+    left as i32
 }
 
 #[test]
 pub fn binary_search_run() {
-    let index = binary_search(&"Rust", &vec!["Python", "Php", "Java", "C", "C++", "Rust"]);
+    let index = binary_search(&"C++", &vec!["C", "C++", "Java", "PHP", "Python", "Rust"]);
     println!("Position: {}", index);
 
-    let index = binary_search(&25, &vec![25, 62, 29, 43, 77]);
+    let index = binary_search(&62, &vec![25, 29, 43, 62, 77]);
     println!("Position: {}", index);
 
-    let index = binary_search(&855, &vec![25, 62, 29, 43, 77]);
+    let index = binary_search(&855, &vec![25, 29, 43, 62, 77]);
     println!("Position: {}", index);
 }
 
 #[test]
 pub fn left_bound_test() {
     let i = left_bound(&100, &vec![1, 2, 2, 3, 3, 3, 3, 4, 5, 6]);
-    assert_eq!(i, -1)
+    assert_eq!(i, -1);
+    let i2 = left_bound(&2, &vec![0, 2, 2, 3, 3, 3, 3, 4, 5, 6]);
+    assert_eq!(i2, 1);
 }
